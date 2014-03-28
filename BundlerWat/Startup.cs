7ff38eba804bebf06e675bfae14d76d749c7f04d
David Ruttka <david@davidruttka.com>
2014-03-27 18:10:17 -0700

    Wat

---
+++ BundlerWat/Startup.cs
@@ -0,0 +1,19 @@
+﻿using BundlerMiddleware;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BundlerWat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            BundlerRoutes.Routes.FromVirtualPath("/", "~/default.html");
+            app.UseBundlerMiddlewareForIIS();
+        }
+    }
+}
